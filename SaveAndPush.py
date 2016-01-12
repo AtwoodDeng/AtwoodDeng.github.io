f96b75b8fbf4bba042393db56dc452834efdc907
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-01-11 23:46:52 -0800

    lll

--- SaveAndPush.py
+++ SaveAndPush.py
@@ -4,5 +4,4 @@ import sys
 if __name__ == "__main__":
 	os.system("git add -A")
 	os.system("git commit -m " + sys.argv[1] if len(sys.argv) > 1 else "update" )
-	print sys.argv[1] if len(sys.argv) > 1 else "update"
 	os.system("git push")
